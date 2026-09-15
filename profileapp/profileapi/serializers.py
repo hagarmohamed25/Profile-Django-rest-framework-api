aae577dd751971eedfc5437bb91adb2cc5a88ea5
Hagar Mohamed <hagar.m@fairpicture.org>
2026-09-15 16:10:22 +0300

    Add WSGI configuration for profileapp project

---
+++ profileapp/profileapi/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from profileapi import models
+
+class HelloSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=10)
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    #Serializes a user profile object
+
+    class Meta:
+        model = models.UserProfileManager
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
+
+    def create(self, validated_data):
+        """Create and return a new user"""
+        user = models.UserProfileManager.objects.create_user(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password']
+        )
+
+        return user
+
+    def update(self, instance, validated_data):
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+
+        return super().update(instance, validated_data)
---
+++ profileapp/profileapi/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from profileapi import views
+
+router = DefaultRouter()
+router.register('help-viewset', views.HelloViewSet, basename='hello-viewset')
+router.register('profiles', views.UserProfileViewSet)
+
+urlpatterns = [
+    path('hello/', views.HelloApiView.as_view()),
+    path('', include(router.urls))
+]
