818feae576e3106e6f57ed9f5cc67d909d2496d1
Hagar Mohamed <hagar.m@fairpicture.org>
2026-09-16 14:34:10 +0300

    Add UserLoginApiView and update URLs; include auth token app in settings

--- profileapp/profileapi/views.py
+++ profileapp/profileapi/views.py
@@ -2,6 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profileapi import serializers, models, permissions
 
@@ -100,3 +103,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
+
+class UserLoginApiView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
