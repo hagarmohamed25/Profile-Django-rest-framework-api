8497bba2bb12348bb38be74e7304405b0b869364
Hagar Mohamed <hagar.m@fairpicture.org>
2026-09-17 13:27:06 +0300

    Add ProfileFeedItem model, serializer, and viewset; update admin and URLs

--- profileapp/profileapi/admin.py
+++ profileapp/profileapi/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from profileapi import models
 
-# Register your models here.
+admin.site.register(models.UserProfile)
+admin.site.register(models.ProfileFeedItem)
+
--- profileapp/profileapi/urls.py
+++ profileapp/profileapi/urls.py
@@ -5,7 +5,7 @@ from profileapi import views
 router = DefaultRouter()
 router.register('help-viewset', views.HelloViewSet, basename='hello-viewset')
 router.register('profiles', views.UserProfileViewSet)
-
+router.register('feed', views.UserProfileFeedViewSet)
 urlpatterns = [
     path('hello/', views.HelloApiView.as_view()),
     path('login/', views.UserLoginApiView.as_view()),
